8af906dfaf9c698b4d3c3b496c00c35f00e977d5
Aamerov <aamersyrai2000@gmail.com>
2023-08-03 07:52:30 +0300

    Updating React Course Files

---
+++ 03-Udemy Courses/04-ReactJS-Maximilian/Code/Section-4-First-Practice-Project/src/App.js
@@ -0,0 +1,57 @@
+import keyConceptsImage from './assets/images/key-concepts.png';
+import componentsImage from './assets/images/components.png';
+import stateImage from './assets/images/state.png';
+import eventsImage from './assets/images/events.png';
+import Card from './components/layout/Card';
+const concepts = [
+  {
+    id: 1,
+    title: 'Components',
+    image: componentsImage,
+    description:
+      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
+  },
+  {
+    id: 2,
+    title: 'State',
+    image: stateImage,
+    description:
+      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
+  },
+  {
+    id: 3,
+    title: 'Events',
+    image: eventsImage,
+    description:
+      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src={keyConceptsImage} alt="Medal badge with a star" />
+        <h1>Key React Concepts</h1>
+        <p>Selected key React concepts you should know about</p>
+      </header>
+      <ul id="concepts">
+        {concepts.map((concept) => (
+          <Card
+            key={concept.id}
+            title={concept.title}
+            description={concept.description}
+            image={concept.image}
+          />
+        ))}
+        {/* <li className="concept">
+          <img src="TODO: IMAGE" alt="TODO: TITLE" />
+          <h2>TODO: TITLE</h2>
+          <p>TODO: DESCRIPTION</p>
+        </li> */}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
